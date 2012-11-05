71807e2f0a4ad3e53c25227791435477ea08a0eb
xiaomo <wxm4ever@gmail.com>
2012-11-05 23:02:38 +0800

    fix

--- wsgi/EasyNote/app.py
+++ wsgi/EasyNote/app.py
@@ -1,6 +1,6 @@
 #-*- coding: utf-8 -*-
 
-from flask import Flask
+from flask import Flask,redirect
 from master.app import master
 
 SECRET_KEY = '0xABC'
@@ -9,9 +9,9 @@ app = Flask(__name__, static_folder='master/static')
 app.config.from_object(__name__)
 app.register_blueprint(master)
 
-@app.route('/app/test')
-def test():
-    return "HELLO TEST"
+@app.route('/')
+def index():
+    return redirect('/index')
 
 if __name__ == '__main__':
     app.run(debug=True)
