3f72c9153f4d2f7454eda47af68332680dea7ffc
xiaomo <wxm4ever@gmail.com>
2012-11-05 23:15:26 +0800

    test

--- wsgi/EasyNote/master/utils.py
+++ wsgi/EasyNote/master/utils.py
@@ -4,18 +4,11 @@ import os
 import pymongo
 from datetime import datetime
 
-HOST = os.environ['OPENSHIFT_DB_HOST']
-PORT = os.environ['OPENSHIFT_DB_PORT']
-
 def make_datetime():
     return datetime.strftime(datetime.now(), "%Y/%m/%d %H:%M")
 
 def get_connection():
-    try:
-        return pymongo.Connection(HOST, int(PORT)).EasyNote
-    except:
-        assert False, "connection Error"
-        return pymongo.Connection('localhost', int(PORT)).EasyNote
+    return pymongo.Connection().EasyNote
 
 
 if __name__ == '__main__':
