3e0313d90b6d21cb05f5f42675fc44bf675d270a
xiaomo <wxm4ever@gmail.com>
2012-11-05 23:48:09 +0800

    tes

--- wsgi/EasyNote/master/utils.py
+++ wsgi/EasyNote/master/utils.py
@@ -4,16 +4,12 @@ import os
 import pymongo
 from datetime import datetime
 
-MONGO_HOST = '127.9.112.129'
-MONGO_PORT = 27017
-
 def make_datetime():
     return datetime.strftime(datetime.now(), "%Y/%m/%d %H:%M")
 
 def get_connection():
-    db = pymongo.Connection(MONGO_HOST, MONGO_PORT).EasyNote
+    db = pymongo.Connection(os.environ['OPENSHIFT_NOSQL_DB_URL']).EasyNote
     return db
-
 
 if __name__ == '__main__':
     conn = get_connection()
