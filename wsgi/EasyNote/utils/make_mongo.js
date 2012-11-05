7806dcce6c3339220e8078b2ff57c778ed682300
xiaomo <wxm4ever@gmail.com>
2012-11-05 23:53:25 +0800

    add openshift

--- wsgi/EasyNote/utils/make_mongo.js
+++
@@ -1,6 +0,0 @@
-var db = connect('EasyNote');
-db.notes.save({text:'This is My First Note', date:(new Date()).toLocaleString()})
-db.notes.save({text:'This is My Second Note', date:(new Date()).toLocaleString()})
-db.notes.save({text:'This is My Third Note', date:(new Date()).toLocaleString()})
-db.notes.save({text:'This is My Firth Note', date:(new Date()).toLocaleString()})
-db.notes.save({text:'This is My Fifth Note', date:(new Date()).toLocaleString()})
