39e9a4c2724f364ac333782cbb08ec7ada170f19
xiaomo <wxm4ever@gmail.com>
2012-11-05 23:06:35 +0800

    test

--- wsgi/EasyNote/app.py
+++ wsgi/EasyNote/app.py
@@ -1,17 +1,17 @@
 #-*- coding: utf-8 -*-
 
 from flask import Flask,redirect
-from master.app import master
+#from master.app import master
 
 SECRET_KEY = '0xABC'
 
 app = Flask(__name__, static_folder='master/static')
 app.config.from_object(__name__)
-app.register_blueprint(master)
+#app.register_blueprint(master)
 
 @app.route('/')
 def index():
-    return redirect('/index')
+    return "HELLO"
 
 if __name__ == '__main__':
     app.run(debug=True)
